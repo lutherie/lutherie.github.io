391d5a43d62845ee9d7e60b4f80a21a9799867d9
Clement Denis <le.mikmac@gmail.com>
2019-03-26 23:37:36 +0000

    push pre version

--- script.js
+++ script.js
@@ -1,58 +1,108 @@
-// generate menu
-const cutAt = (str, sep) => {
-  const pos = str.lastIndexOf(sep)
-  return [str.slice(0, pos), str.slice(pos + 1)]
+const dbp = new Promise((resolve, reject) => {
+  const openreq = window.indexedDB.open('page-cache', 1)
+  openreq.onerror = () => reject(openreq.error)
+  openreq.onsuccess = () => resolve(openreq.result)
+  openreq.onupgradeneeded = () => openreq.result.createObjectStore('idb')
+})
+
+const call = async (type, method, ...args) => {
+  const db = await dbp
+  const transaction = db.transaction('idb', type)
+  const store = transaction.objectStore('idb')
+
+  return new Promise((resolve, reject) => {
+    const req = store[method](...args)
+    transaction.oncomplete = () => resolve(req)
+    transaction.onabort = transaction.onerror = () => reject(transaction.error)
+  })
 }
-const menu = [
-  ...index
-    .reduce((acc, item) => {
-      const [_path, _name] = cutAt(item.file, '/')
-      const path = _path
-        .split('/')
-        .filter(Boolean)
-        .join(' ')
-      acc.has(path) || acc.set(path, [])
-      const cat = acc.get(path)
-      const [name, ext] = cutAt(_name, '.')
-      const [uri] = cutAt(item.file, '.')
-      cat.push({ ...item, name, ext, cat: path, uri })
-      return acc
-    }, new Map())
-    .entries(),
-]
 
-const category = ([cat, items]) => `<li>
-  <span class="category">${cat}</span>
-  <ul>
-    ${items
-      .map(item => `<li><a href="#${item.uri}">${item.name}</a></li>`)
-      .join('')}
-  </ul>
-</li>
-`
+const get = async key => (await call('readonly', 'get', key)).result
+const set = (key, value) =>
+  value === undefined
+    ? call('readwrite', 'delete', key)
+    : call('readwrite', 'put', value, key)
 
-const _id = document.getElementById.bind(document)
-_id('nav').innerHTML = menu.map(category).join('')
+const getContent = async name => {
+  const path = `/pages/${encodeURIComponent(name)}`
+  const res = await fetch(path)
+  const text = await res.text()
+  const dom = new DOMParser().parseFromString(text, 'text/html')
+  const elem = dom.body.firstElementChild
+  if (!elem) return ''
+  elem.firstElementChild && elem.firstElementChild.remove()
+  return elem.innerHTML.trim()
+}
 
-const articleEl = _id('article')
-articleEl.innerHTML = snarkdown(_id('main-content').innerHTML)
+const parseName = src => {
+  const [, index, locale, title] = src.split(/([0-9]{2})-([a-z]{2})/)
+  const name = src.slice(0, -3)
+  return {
+    index: Number(index),
+    locale,
+    title: title.trim().slice(0, -3),
+    name,
+    search: `?${new URLSearchParams({ page: name })}`,
+  }
+}
 
-const loadHash = async hash => {
-  if (hash === '#') {
-    articleEl.innerHTML = snarkdown(_id('main-content').innerHTML)
-    return (document.title = 'Lutherie')
+const displayPage = innerHTML => {
+  if (!innerHTML) return
+  document.getElementById('article').innerHTML = innerHTML
+}
+
+const queryPages = fetch(
+  'https://api.github.com/repos/lutherie/lutherie.github.io/contents/pages',
+)
+
+const generateMenu = pages => {
+  console.log(pages)
+  document.getElementById('nav').innerHTML = pages
+    .sort((a, b) => a.index - b.index)
+    .map(({ title, search }) => `<li><a href="./${search}">${title}</a></li>`)
+    .join('\n')
+}
+
+const loadContent = async ([cachedPages = {}, locale = 'fr']) => {
+  // init pages from cache
+  const { searchParams } = new URL(window.location)
+  const selectedPage = searchParams.get('page')
+
+  generateMenu(Object.values(cachedPages))
+  if (selectedPage) {
+    const page = cachedPages[selectedPage]
+    displayPage(await (page ? get(page.sha) : getContent(selectedPage)))
   }
 
-  if (!hash || hash.length < 3) return (location.hash = '#')
-  const res = await fetch(`${hash.slice(1)}.md`)
-  if (res.status !== 200) return (location.hash = '#')
-  articleEl.innerHTML = snarkdown(await res.text())
-  document.title = hash
-    .slice(1)
-    .split('/')
-    .filter(Boolean)
-    .join(' ')
+  const res = await queryPages
+  const pages = (await res.json())
+    .filter(p => p.type === 'file' && p.name.endsWith('.md'))
+    .map(({ sha, name }) => ({ ...parseName(name), sha }))
+
+  const work = []
+  const cache = pages.reduce((a, { name, ...p }) => ({ ...a, [name]: p }), {})
+  work.push(set('pages', cache))
+
+  for (const page of pages) {
+    const cachedPage = cachedPages[page.name]
+    const cachedSha = cachedPage && cachedPage.sha
+    if (page.sha !== cachedSha) {
+      const content = (await getContent(page.name)) || page.name
+      if (page.name === selectedPage) {
+        displayPage(content)
+      }
+      work.push(set(page.sha, content))
+      cachedSha && work.push(set(cachedSha, undefined))
+    }
+  }
+
+  generateMenu(pages)
+
+  return Promise.all(work)
 }
 
-addEventListener('hashchange', e => loadHash(new URL(e.newURL).hash))
-loadHash(window.location.hash)
+const start = Date.now()
+Promise.all([get('pages'), get('locale')])
+  .then(loadContent)
+  .then(console.log)
+  .catch(console.error)
