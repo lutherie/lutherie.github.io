315bb43f1327b5c36cb702ea48cf1af06e27fd21
Clement Denis <le.mikmac@gmail.com>
2019-03-30 16:37:11 +0000

    add locale

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let CACHED_PAGES = {}
-let CURRENT_PAGE_NAME = ''
 
 const dbp = new Promise((resolve, reject) => {
   const openreq = window.indexedDB.open('page-cache', 1)
@@ -63,34 +62,38 @@ const displayPage = innerHTML => {
   }
 }
 
-const queryPages = fetch(
-  'https://api.github.com/repos/lutherie/lutherie.github.io/contents/pages',
-)
-
-const generateMenu = pages => {
-  document.getElementById('nav').innerHTML = pages
-    .sort((a, b) => a.index - b.index)
-    .map(
-      p =>
-        `<li><a data-page=${encodeURIComponent(p.name)} href="${
-          location.pathname
-        }${p.search}">${p.title}</a></li>`,
-    )
-    .join('\n')
+const generateMenu = (pages, locale = 'en') => {
+  document.getElementById('nav').innerHTML =
+    pages
+      .filter(p => p.locale === locale)
+      .sort((a, b) => a.index - b.index)
+      .map(
+        p =>
+          `<li><a data-page=${encodeURIComponent(p.name)} href="${
+            location.pathname
+          }${p.search}">${p.title}</a></li>`,
+      )
+      .join('\n') +
+    `<li><a href="https://traitedelutherie.com">Traite de Lutherie</a></li>`
 }
 
 const loadPage = name => {
   const page = CACHED_PAGES[name]
   if (!page) return page
+
+  const locale = name ?  name.split(/[0-9]{2}-([a-z]{2})/)[1] : 'en'
+
+  const localeButton = document.getElementById(locale)
+  localeButton.disabled = true
+  localeButton.alt.disabled = false
+
   get(page.sha)
     .then(async actualContent => {
       displayPage(actualContent)
       const content = await getContent(name)
       if (actualContent !== content) {
-        // show refresh ??
-        // update sha
-        // update cache
         displayPage(content)
+        return loadContent()
       }
     })
     .catch(console.error)
@@ -100,6 +103,7 @@ const loadPage = name => {
 
 window.addEventListener('click', async e => {
   if (e.button || e.buttons) return
+  console.dir(e.target)
   if (!e.target.dataset.page) return
   const clickedName = decodeURIComponent(e.target.dataset.page)
   const currentName = new URL(location).searchParams.get('page')
@@ -117,27 +121,62 @@ window.addEventListener('popstate', async e => {
   loadPage(new URL(location).searchParams.get('page'))
 })
 
-const loadContent = async ([cachedPages = {}, locale = 'fr']) => {
+let queryPages
+
+const loadContent = () => {
+  queryPages = fetch(
+    'https://api.github.com/repos/lutherie/lutherie.github.io/contents/pages',
+  )
+
+  return get('pages')
+    .then(handleLoading)
+    .then(console.log)
+    .catch(console.error)
+}
+
+const handleLoading = async (cachedPages = {}) => {
   // init pages from cache
   const { searchParams } = new URL(location)
   const selectedPage = searchParams.get('page')
+  const locale = selectedPage ?  selectedPage.split(/[0-9]{2}-([a-z]{2})/)[1] : 'en'
+
+  const localeButton = document.getElementById(locale)
+  localeButton.disabled = true
+  localeButton.alt.disabled = false
 
   CACHED_PAGES = cachedPages
   generateMenu(Object.values(cachedPages))
   if (selectedPage) {
     const page = cachedPages[selectedPage]
     displayPage(await (page ? get(page.sha) : getContent(selectedPage)))
+
+    console.log('show page', { page })
+  } else {
+    console.log('no selectedPage')
   }
 
   const res = await queryPages
   res.ok || console.error(Error('Github request failed'))
-  const pages = (res.ok ? (await res.json()) : [])
+  const pages = (res.ok ? await res.json() : [])
     .filter(p => p.type === 'file' && p.name.endsWith('.md'))
     .map(({ sha, name }) => ({ ...parseName(name), sha }))
 
   const work = []
-  const cache = pages.reduce((a, { name, ...p }) => ({ ...a, [name]: p }), {})
+  const cache = pages.reduce(
+    (a, page) => ({
+      ...a,
+      [page.name]: {
+        ...page,
+        ...pages
+          .filter(p => p.index === page.index)
+          .reduce((b, p) => ({ ...b, [p.locale]: p.name }), {}),
+      },
+    }),
+    {},
+  )
   work.push(set('pages', cache))
+
+  console.log(cache)
 
   for (const page of pages) {
     const cachedPage = cachedPages[page.name]
@@ -152,14 +191,35 @@ const loadContent = async ([cachedPages = {}, locale = 'fr']) => {
     }
   }
 
-  generateMenu(pages)
-
+  generateMenu(pages, locale)
   CACHED_PAGES = cache
 
   return Promise.all(work)
 }
 
-Promise.all([get('pages'), get('locale')])
-  .then(loadContent)
-  .then(console.log)
-  .catch(console.error)
+// Load initial content
+loadContent()
+
+const enButton = document.getElementById('en')
+const frButton = document.getElementById('fr')
+enButton.alt = frButton
+frButton.alt = enButton
+enButton.addEventListener('click', e => switchLocale('en'))
+frButton.addEventListener('click', e => switchLocale('fr'))
+
+const locales = { fr: frButton, en: enButton }
+const altLocales = { en: frButton, fr: enButton }
+
+const switchLocale = async locale => {
+  const currentName = new URL(location).searchParams.get('page')
+  const currentPage = CACHED_PAGES[currentName] || Object.keys(CACHED_PAGES)[0]
+  if (currentPage.locale === locale) return
+  const nextPage = await loadPage(currentPage[locale])
+  console.log('switching to', { locale, currentName, currentPage, nextPage })
+  if (!nextPage) {
+    console.error('no alternate page', currentPage, locale)
+  }
+  generateMenu(Object.values(CACHED_PAGES), locale)
+  history.pushState(null, nextPage.name, nextPage.search)
+  return 
+}
